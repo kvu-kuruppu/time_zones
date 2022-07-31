ab0e8ec38ead1dd9e02f3dd4e4808ed323620a27
Your Name <Your Email>
2022-07-31 23:55:38 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/time_zones/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.time_zones
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:time_zones/pages/home.dart';
+import 'package:time_zones/pages/loading.dart';
+import 'package:time_zones/pages/choose_location.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
+  ));
+}
